ef288fd213f95a90351242cd86fb6a93b09f1e45
ikReza <ireza.kaiser00@gmail.com>
2020-11-07 21:55:01 +0600

    setup done

---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hi from page-3</h1>
+    <Link to="/">Go to home</Link>
+  </Layout>
+)
+
+export default ThirdPage
