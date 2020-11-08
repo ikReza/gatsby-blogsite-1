ae05dbf7a138be48a2d6ff9446bcab7276fd9ec6
ikReza <ireza.kaiser00@gmail.com>
2020-11-08 20:14:34 +0600

    total posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default ({ data }) => (
     <SEO title="Home" />
     <div>
       <h1>ikr's thoughts</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>Total posts: {data.allMarkdownRemark.totalCount}</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <BlogLink to={node.fields.slug}>
